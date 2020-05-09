52a8ba55e2536c5e8d9eabf43e988c567fa646a4
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-05-09 18:25:13 -0300

    Melhorando o menu e apresentação dados

--- lib/desafio1.rb
+++ lib/desafio1.rb
@@ -34,72 +34,87 @@ when "0"
   puts "+------------------------------------+" 
   puts
 when "1"  
-  puts "\n \nDigite o primeiro nome para pesquisar: " 
+  print "\n \t Digite o primeiro nome para pesquisar: " 
   nome = gets.chomp
+  puts
   groupBy = "UF"
   url_nomes = "#{api_base_url_nome}#{nome}?groupBy=#{groupBy}"
   resource_nome = RestClient::Resource.new(url_nomes)
   rest_nome = resource_nome.get
   rest_nome_json = JSON.parse(rest_nome, :symbolize_names => true)
-  puts "Ranking para o nome: #{nome} em Localidades, Frequencia, População e Proporção"
+  puts "\t Ranking para #{nome} em Localidades, Frequencia, População e Proporção"
+  puts
   rest_nome_json.each do |ranking|
-    puts "Localidade: #{ranking[:localidade]} - Frequêcia: #{ranking[:res][0][:frequencia]} - População: #{ranking[:res][0][:populacao]} - Proporção: #{ranking[:res][0][:proporcao]}"
+    puts "\t Localidade: #{ranking[:localidade]} - Frequêcia: #{ranking[:res][0][:frequencia]} - População: #{ranking[:res][0][:populacao]} - Proporção: #{ranking[:res][0][:proporcao]}"
   end 
+  puts
     
 when "2" 
-  print "\n \nDigite uma década para pesquisar: " 
+  print "\n \t Digite uma década para pesquisar: " 
   decada = gets.chomp
+  puts
   url_ranking = "#{api_base_url_ranking}?decada=#{decada}"
   resource_ranking = RestClient::Resource.new(url_ranking)
   rest_ranking = resource_ranking.get
   rest_ranking_json = JSON.parse(rest_ranking, :symbolize_names => true)
-  puts "Ranking de Nomes por Década"
+  puts "\t Ranking de Nomes por Década"
+  puts
   rest_ranking_json[0][:res].each do |ranking|
-    puts "#{ranking[:ranking]}. #{ranking[:nome]} - Frequência: #{ranking[:frequencia]}"
+    puts "\t #{ranking[:ranking]}. #{ranking[:nome]} - Frequência: #{ranking[:frequencia]}"
   end
+  puts
   
 when "3"  
-  print "\n \n Digite um sexo para pesquisar" 
-  puts  "\n \n'M' para Masculino 'F' para FEMININO:" 
+  puts "\n \t Digite um sexo para pesquisar" 
+  print  "\n \t 'M' para Masculino ou 'F' para FEMININO: " 
   sexo = gets.chomp
+  puts
   url_ranking = "#{api_base_url_ranking}?sexo=#{sexo}"
   resource_sexo = RestClient::Resource.new(url_ranking)
   rest_sexo = resource_sexo.get
   rest_sexo_json = JSON.parse(rest_sexo, :symbolize_names => true)
-  puts "Ranking de Nome por Sexo todos Periodos"
+  puts "\t Ranking de Nome por Sexo "
+  puts
   rest_sexo_json[0][:res].each do |sexo|
-    puts "Periodo: #{sexo[:nome]} - Frequência: #{sexo[:frequencia]}"
+    puts "\t #{sexo[:ranking]}. Nome: #{sexo[:nome]} - Frequência: #{sexo[:frequencia]}"
   end
+  puts
 
 when "4"
-  puts "\n \nDigite o primeiro nome para pesquisar: " 
+  print "\n \tDigite o primeiro nome para pesquisar: " 
   nome = gets.chomp 
-  puts  "\n \nDigite'M' para Masculino 'F' para FEMININO:" 
+  print  "\n \tDigite'M' para Masculino 'F' para FEMININO: " 
   sexo = gets.chomp
+  puts
   url_nomes = "#{api_base_url_nome}#{nome}?sexo=#{sexo}"
   resource_periodo = RestClient::Resource.new(url_nomes)
   rest_periodo_decada = resource_periodo.get
   rest_periodo_decada_json = JSON.parse(rest_periodo_decada, :symbolize_names => true)
   tam_periodo_res = rest_periodo_decada_json[0][:res].size
-  puts "Consultando Nome por Sexo nos Periodos"
+  puts "\t Consultando #{nome} por Sexo nos Periodos"
+  puts
   rest_periodo_decada_json[0][:res].each do |decada|
-    puts "Periodo: #{decada[:periodo]} - Frequência: #{decada[:frequencia]}"
+    puts "\t Periodo: #{decada[:periodo]} - \t Frequência: #{decada[:frequencia]}"
   end
+  puts
 
 
 when "5"
-  puts "\n \nDigite o primeiro nome em um estado: " 
+  print "\n \t Digite o primeiro nome em um estado: " 
   nome = gets.chomp 
-  puts  "\n \nDigite um Estado para esquisar. Ex.: '33' para Rio de Janeiro:" 
+  print "\n \t Digite um Estado para esquisar. Ex.: '33' para Rio de Janeiro: " 
   localidade = gets.chomp
+  puts
   url_nome_localidade = "#{api_base_url_nome}#{nome}?localidade=#{localidade}"
   resource_nome_localidade = RestClient::Resource.new(url_nome_localidade)
   rest_nome_localidade = resource_nome_localidade.get
   rest_nome_localidade_json = JSON.parse(rest_nome_localidade, :symbolize_names => true)
-  puts "Consultando Nome por Localidade nos Periodos"
+  puts "\t Consultando Nome por Estado nos Periodos"
+  puts
   rest_nome_localidade_json[0][:res].each do |nome|
-    puts "Periodo: #{nome[:periodo]} - Frequência: #{nome[:frequencia]}"
+    puts "\t Periodo: #{nome[:periodo]} - \t Frequência: #{nome[:frequencia]}"
   end
+  puts
 
 when "6"
   resource_localidade = RestClient::Resource.new(api_base_url_localidade)
@@ -107,9 +122,11 @@ when "6"
   rest_localidade_json = JSON.parse(rest_localidade, :symbolize_names => true)
   
   puts "\n \n \t \t Consultando siglas das localidades"
-  rest_localidade_json.each do |uf|
-  puts "\t \t #{uf[:nome]} " + '-' + " #{uf[:sigla]}"
+  puts
+  rest_localidade_json.each do |localidade|
+  puts "\t \t #{localidade[:id]} - #{localidade[:sigla]} - #{localidade[:nome]} "
   end
+  puts
 
 else  
   puts "\t \n Escolha uma das opções..."
