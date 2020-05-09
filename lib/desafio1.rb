5a2f95bd7eecbd12b34437ae510c54cad4fe9be7
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-05-09 09:48:56 -0300

    Adicionando busca por UF, sem tratar siglas

--- lib/desafio1.rb
+++ lib/desafio1.rb
@@ -4,6 +4,7 @@ require 'json'
 
 api_base_url_nome = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/"
 api_base_url_ranking = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/ranking/"
+api_base_url_localidade = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"
 
 system("clear")
 puts
@@ -13,6 +14,9 @@ puts "|                                    |"
 puts "| 1- Pesquisar por Primeiro nome     |" 
 puts "| 2- Digitar uma década ex. 1950     |" 
 puts "| 3- Pesquisar por sexo: M ou F      |" 
+puts "| 4- Nome por Sexo nas décadas       |" 
+puts "| 5- Nome por Estados                |" 
+puts "| 6- Consultar UF dos estados        |" 
 puts "|                                    |" 
 puts "| 0- Sair                            |" 
 puts "|                                    |" 
@@ -79,6 +83,77 @@ when "3"
   puts "\nNomes por sexo: \n " 
   print sexo
   puts
+
+when "4"
+  puts "\n \nDigite o primeiro nome para pesquisar: " 
+  nome = gets.chomp 
+  puts  "\n \nDigite'M' para Masculino 'F' para FEMININO:" 
+  sexo = gets.chomp
+  #url_ranking = "#{api_base_url_ranking}?sexo=#{sexo}"
+
+  url_nomes = "#{api_base_url_nome}#{nome}?sexo=#{sexo}"
+
+  resource_periodo = RestClient::Resource.new(url_nomes)
+  rest_periodo_decada = resource_periodo.get
+  rest_periodo_decada_json = JSON.parse(rest_periodo_decada, :symbolize_names => true)
+  tam_periodo_res = rest_periodo_decada_json[0][:res].size
+  periodo = []
+  start_periodo = 0
+  while tam_periodo_res > start_periodo
+    periodo << rest_periodo_decada_json[0][:res][start_periodo][:periodo]
+    periodo << rest_periodo_decada_json[0][:res][start_periodo][:frequencia]
+               
+    start_periodo += 1
+  end
+  periodo.to_s
+  print periodo
+
+  index = 0
+  while periodo.size > index
+    puts periodo[index]
+    #puts periodo[index + 1]
+    index += 1
+  end
+  
+
+  resource_nome = RestClient::Resource.new(url_nomes)
+  rest_nome = resource_nome.get
+  rest_nome_json = JSON.parse(rest_nome, :symbolize_names => true)
+  puts "\n Apenas uma Nome todos dados do JSON: \n \n #{rest_nome_json[0].to_s}\n"
+  puts
+
+when "5"
+
+
+  puts "\n \nDigite o primeiro nome em um estado: " 
+  nome = gets.chomp 
+  puts  "\n \nDigite um Estado para esquisar. Ex.: '33' para Rio de Janeiro:" 
+  localidade = gets.chomp
+
+  url_localidade = "#{api_base_url_nome}#{nome}?localidade=#{localidade}"
+
+  resource_localidade = RestClient::Resource.new(url_localidade)
+  rest_localidade = resource_localidade.get
+  rest_localidade_json = JSON.parse(rest_localidade, :symbolize_names => true)
+  tam_rest_localidade = rest_localidade_json[0][:res].size
+  localidade = []
+  start_localidade = 0
+  while tam_rest_localidade > start_localidade
+    localidade << rest_localidade_json[0][:res][start_localidade][:periodo]
+    localidade << rest_localidade_json[0][:res][start_localidade][:frequencia]
+               
+    start_localidade += 1
+  end
+  localidade.to_s
+  print localidade
+
+  index = 0
+  while localidade.size > index
+    puts localidade[index]
+    index += 1
+  end
+
+
 
 else  
   puts "\t \n Escolha uma das opções..."
