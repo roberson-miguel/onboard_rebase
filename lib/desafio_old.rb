7ac396fe3c1ff6c3f1a8a746fad50f751b290997
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-05-10 06:13:44 -0300

    separando em metodos

--- lib/desafio_old.rb
+++ lib/desafio_old.rb
@@ -1,69 +1,46 @@
 require 'rest-client'
 require 'json'
 
-def set_player_mode
-  system("clear")
-  puts 
-  print "\t Escolha uma das opções: \n " 
-  puts 
-  puts
-  puts
-  puts "\t \t 1- Pesquisar Nome no ranking geral \n " 
-  puts "\t \t 2- Digitar uma década ex. 1950 \n " 
-  puts "\t \t 3- Pesquisar por sexo: M ou F \n " 
-  puts "\t \t 4- Nome por Sexo nas décadas \n " 
-  puts "\t \t 5- Nome por Estados \n " 
-  puts "\t \t 6- Consultar UF dos estados \n " 
-  puts 
-  puts "\t \t 0- SAIR" 
-  puts 
-  puts
-  print "\t Digite sua escolha: " 
-  while true do
-    escolha = gets.chomp.to_s
-    if escolha == "1" then
-      return nome 
-    elsif escolha == "2" then
-      return decada
-    elsif escolha == "3" then
-      return sexo
-    elsif escolha == "4" then
-      return nome_sexo
-    elsif escolha == "5" then
-      return nome_local
-    elsif escolha == "6" then
-      return local
-    elsif escolha == "0" then
-      return sair
-    else
-      puts
-      puts
-      puts "\t \t Escolha uma das opções..."
-      puts
-      sleep 1.9
-      return set_player_mode
-      #system("ruby desafio_def.rb")
-    end
-  end
-end
+api_base_url_nome = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/"
+api_base_url_ranking = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/ranking/"
+api_base_url_localidade = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
 
-def sair 
+system("clear")
+puts 
+print "\t Escolha uma das opções: \n " 
+puts 
+puts
+puts
+puts "\t \t 1- Pesquisar Nome no ranking geral \n " 
+puts "\t \t 2- Digitar uma década ex. 1950 \n " 
+puts "\t \t 3- Pesquisar por sexo: M ou F \n " 
+puts "\t \t 4- Nome por Sexo nas décadas \n " 
+puts "\t \t 5- Nome por Estados \n " 
+puts "\t \t 6- Consultar UF dos estados \n " 
+puts 
+puts "\t \t 0- SAIR" 
+puts 
+puts
+print "\t Digite sua escolha: " 
+escolha = gets.chomp 
+
+
+
+case escolha
+when "0"
   puts 
   puts "+------------------------------------+" 
   puts "|                                    |" 
-  puts "|        Saindo...Obrigado...        |"
+  puts "|     Saindo...Obrigado...           |"
   puts "|                                    |" 
   puts "+------------------------------------+" 
   puts
-  sleep 1.9
-end
-
-def nome  
+when "1"  
   print "\n \t Digite o primeiro nome para pesquisar: " 
   nome = gets.chomp
   puts
   groupBy = "UF"
-  url_nomes = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/#{nome}?groupBy=#{groupBy}"
+  url_nomes = "#{api_base_url_nome}#{nome}?groupBy=#{groupBy}"
   resource_nome = RestClient::Resource.new(url_nomes)
   rest_nome = resource_nome.get
   rest_nome_json = JSON.parse(rest_nome, :symbolize_names => true)
@@ -72,13 +49,13 @@ def nome
   rest_nome_json.each do |ranking|
     puts "\t Localidade: #{ranking[:localidade]} - Frequêcia: #{ranking[:res][0][:frequencia]} - População: #{ranking[:res][0][:populacao]} - Proporção: #{ranking[:res][0][:proporcao]}"
   end 
-end
+  puts
     
-def decada 
+when "2" 
   print "\n \t Digite uma década para pesquisar: " 
   decada = gets.chomp
   puts
-  url_ranking = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/ranking/?decada=#{decada}"
+  url_ranking = "#{api_base_url_ranking}?decada=#{decada}"
   resource_ranking = RestClient::Resource.new(url_ranking)
   rest_ranking = resource_ranking.get
   rest_ranking_json = JSON.parse(rest_ranking, :symbolize_names => true)
@@ -87,14 +64,14 @@ def decada
   rest_ranking_json[0][:res].each do |ranking|
     puts "\t #{ranking[:ranking]}. #{ranking[:nome]} - Frequência: #{ranking[:frequencia]}"
   end
-end
+  puts
   
-def sexo
+when "3"  
   puts "\n \t Digite um sexo para pesquisar" 
   print  "\n \t 'M' para Masculino ou 'F' para FEMININO: " 
   sexo = gets.chomp
   puts
-  url_ranking = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/ranking/?sexo=#{sexo}"
+  url_ranking = "#{api_base_url_ranking}?sexo=#{sexo}"
   resource_sexo = RestClient::Resource.new(url_ranking)
   rest_sexo = resource_sexo.get
   rest_sexo_json = JSON.parse(rest_sexo, :symbolize_names => true)
@@ -103,15 +80,15 @@ def sexo
   rest_sexo_json[0][:res].each do |sexo|
     puts "\t #{sexo[:ranking]}. Nome: #{sexo[:nome]} - Frequência: #{sexo[:frequencia]}"
   end
-end
+  puts
 
-def nome_sexo
+when "4"
   print "\n \tDigite o primeiro nome para pesquisar: " 
   nome = gets.chomp 
   print  "\n \tDigite'M' para Masculino 'F' para FEMININO: " 
   sexo = gets.chomp
   puts
-  url_nomes = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/#{nome}?sexo=#{sexo}"
+  url_nomes = "#{api_base_url_nome}#{nome}?sexo=#{sexo}"
   resource_periodo = RestClient::Resource.new(url_nomes)
   rest_periodo_decada = resource_periodo.get
   rest_periodo_decada_json = JSON.parse(rest_periodo_decada, :symbolize_names => true)
@@ -121,12 +98,11 @@ def nome_sexo
   rest_periodo_decada_json[0][:res].each do |decada|
     puts "\t Periodo: #{decada[:periodo]} - \t Frequência: #{decada[:frequencia]}"
   end
-end
+  puts
 
 
-def nome_local
+when "5"
   system("clear")
-  api_base_url_localidade = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
   resource_localidade = RestClient::Resource.new(api_base_url_localidade)
   rest_localidade = resource_localidade.get
   rest_localidade_json = JSON.parse(rest_localidade, :symbolize_names => true)
@@ -142,7 +118,7 @@ def nome_local
   print "\t Exemplo, digite '33' para Rio de Janeiro: " 
   localidade = gets.chomp
   puts
-  url_nome_localidade = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/#{nome}?localidade=#{localidade}"
+  url_nome_localidade = "#{api_base_url_nome}#{nome}?localidade=#{localidade}"
   resource_nome_localidade = RestClient::Resource.new(url_nome_localidade)
   rest_nome_localidade = resource_nome_localidade.get
   rest_nome_localidade_json = JSON.parse(rest_nome_localidade, :symbolize_names => true)
@@ -151,10 +127,9 @@ def nome_local
   rest_nome_localidade_json[0][:res].each do |nome|
     puts "\t Periodo: #{nome[:periodo]} - \t Frequência: #{nome[:frequencia]}"
   end
-end
+  puts
 
-def local
-  api_base_url_localidade = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+when "6"
   resource_localidade = RestClient::Resource.new(api_base_url_localidade)
   rest_localidade = resource_localidade.get
   rest_localidade_json = JSON.parse(rest_localidade, :symbolize_names => true)
@@ -164,6 +139,11 @@ def local
   rest_localidade_json.each do |localidade|
   puts "\t \t #{localidade[:id]} - #{localidade[:sigla]} - #{localidade[:nome]} "
   end
-end 
+  puts
 
-set_player_mode
+else  
+  puts "\t \n Escolha uma das opções..."
+  puts
+  sleep 1.9
+  system("ruby desafio_old.rb")
+end  
