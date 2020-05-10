a70a6049bb410717324d3f1012657ec9370f4bf8
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-05-10 12:57:48 -0300

    Em construção tests Menu

--- spec/exibe_menu_spec.rb
+++ spec/exibe_menu_spec.rb
@@ -1,6 +1,8 @@
 describe 'Exibe Menu ' do
-     it 'retorna a escolha do usuario caso seja válida' do
-       allow($stdin).to receive(:gets).and_return("1\n", "Maria\n")
-       expect { load "./lib/desafio1.rb" }.to output("Escolha uma das opções:").to_stdout
-     end
+    it 'retorna a escolha do usuario caso seja válida' do
+      allow($stdin).to receive(:gets).and_return("1\n", "Maria\n")
+      expect { load "./lib/desafio1.rb" }
+      expect ( output "Esse programa traz um ranking").to_stdout
+      expect ("Esse programa traz um ranking").to_str
+    end
 end
