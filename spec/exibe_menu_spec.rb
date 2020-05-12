c75ef06cf6e8e83f979da6d3aa28353c0a52724e
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-05-12 11:00:52 -0300

    Ainda tentando sair dos testes :(

--- spec/exibe_menu_spec.rb
+++ spec/exibe_menu_spec.rb
@@ -1,9 +1,12 @@
+require "spec_helper"
+require 'rest-client'
+
 describe 'Exibe Menu ' do
   it 'retorna a escolha do usuario caso seja válida' do
     allow($stdin).to receive(:gets).and_return("1\n", "Maria\n")
-    expect { load "./lib/desafio1.rb" }
+   
     expect ( output "Esse programa traz um ranking").to_stdout
-    expect ("Esse programa traz um ranking").to_str
+    
   end
 end
 
